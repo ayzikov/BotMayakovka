e570059a98fcdcd85c64dce495b5e24d0630a1f4
RevelRies <ayzikov070@mail.ru>
2023-05-18 22:01:31 +0300

    второй этап реализации статистики

--- Bot/crud_statistics.py
+++ Bot/crud_statistics.py
@@ -7,19 +7,58 @@ from dotenv import load_dotenv
 from aiohttp import ClientSession
 
 
-async def action(tg_id: int, full_name: str, username: str, btn_name: str):
+async def action(tg_id: int, full_name: str, username: str, msg_name: str, msg_id: int):
     '''
     Функция вызывается когда пользователь нажимает на любую кнопку в боте
-    :param btn_name: название нажатой кнопки
+    :param tg_id: id пользователя
+    :param full_name: полное имя
+    :param username: юзернейм
+    :param msg_name: название кнопки на которую нажал пользователь
+    :param msg_id: id сообщения
     :return:
     '''
 
     # из БД запришиваются id всех пользователей и если id этого пользователя не присутствует в БД, то создается новый пользователь
+    # запрос возвращает True если пользователь существует и False в противном случае
+    data = {}
+    json_data = json.dumps(data)
+    async with ClientSession() as session:
+        async with session.get('http://127.0.0.1:8000/user/', data=json_data) as response:
+
+            # если пользователя нет, то создаем
+            if not response:
+                data = {'tg_id': tg_id, 'full_name': full_name, 'username': username}
+                json_data = json.dumps(data)
+                async with ClientSession() as session:
+                    async with session.post('http://127.0.0.1:8000/user/', data=json_data) as response:
+
+                        print(response)
 
 
+            # если пользователь уже был, то обновляем ему last_time
+            else:
+                data = {'tg_id': tg_id}
+                json_data = json.dumps(data)
+                async with ClientSession() as session:
+                    async with session.put('http://127.0.0.1:8000/user/', data=json_data) as response:
 
-    # если пользователь уже был, то обновляем ему last_time
+                        print(response)
+
 
     # сюда передается id пользователя, id сообщения и название кнопки
     # оно уходит во вьюхи
     # получаем из БД объект пользователя и создаем новый объект Action
+    data = {'tg_id': tg_id, 'msg_name': msg_name, 'msg_id': msg_id}
+    json_data = json.dumps(data)
+    async with ClientSession() as session:
+        async with session.post('http://127.0.0.1:8000/action/', data=json_data) as response:
+
+            print(response)
+
+
+async def statistic(message, msg_name):
+    await action(tg_id=message.from_user.id,
+                 full_name=message.from_user.full_name,
+                 username=message.from_user.username,
+                 msg_id=message.message_id,
+                 msg_name=msg_name)
